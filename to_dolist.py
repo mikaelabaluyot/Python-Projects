a87e87731bddcb0a0e19772df2121945ab283cbc
mikaelabaluyot <150456976+mikaelabaluyot@users.noreply.github.com>
2025-05-08 17:22:08 +0300

    Add files via upload
    for python lists excercise in Codedex

---
+++ to_dolist.py
@@ -0,0 +1,15 @@
+
+#Print the first item and the second item. What did you get?
+
+#Next, use slicing to print the third, fourth, and fifth items.
+
+#Try printing out the item at index 9 to see the IndexError before moving on.
+
+todo_list = ['Get Quarters', 'Do laundry', 'Take a walk', 'Get a haircut', 'Make tea',
+             'Complete lists chapter', 'Call Mom', 'Watch Hero Academia']
+
+print(todo_list[1])
+print(todo_list[2])
+print(todo_list[3:6])
+print(todo_list[9:5])
+
