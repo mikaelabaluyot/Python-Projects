606ad82408da8dad8d5f912b392a8666569bebab
mikaelabaluyot <150456976+mikaelabaluyot@users.noreply.github.com>
2025-05-09 12:43:35 +0300

    Add files via upload
    Creating and using tuples for friends' locations.

---
+++ besties' locations.py
@@ -0,0 +1,10 @@
+bestie1_loc = (56.879635, 24.603189)
+bestie2_loc = (60.205238, 24.654079)
+bestie3_loc = (60.169857, 24.938379)
+all_besties = bestie1_loc + bestie2_loc + bestie3_loc
+
+print("bestie 1: ",bestie1_loc, "bestie 2: " ,bestie2_loc, "bestie 3: ", bestie3_loc)
+
+print("all the besties: ", all_besties)
+
+print("The furthest bestie is: ", bestie1_loc)
